3c99ae23c1cda746c9ff85a29541d5969086cbda
marisolcastrolopez <solcastrodg@gmail.com>
2019-05-21 00:12:00 -0500

    Starting waiter UI. In development

--- src/components/WaiterUI.js
+++ src/components/WaiterUI.js
@@ -1,11 +1,63 @@
 import React from 'react';
 import "antd/dist/antd.css";
+import { Layout, Menu, Icon } from 'antd';
 
-const WaiterUI = () => {
-    return (
-        <h1>Hi dude WaiterUI</h1>
-    )
-    
-}
+const { Header, Sider, Content } = Layout;
+
+class WaiterUI extends React.Component {
+    state = {
+      collapsed: false,
+    };
+  
+    toggle = () => {
+      this.setState({
+        collapsed: !this.state.collapsed,
+      });
+    };
+  
+    render() {
+      return (
+        <Layout className="viewheight">
+          <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">
+                <Icon type="user" />
+                <span>nav 1</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Icon type="video-camera" />
+                <span>nav 2</span>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="upload" />
+                <span>nav 3</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header style={{ background: '#fff', padding: 0 }}>
+              <Icon
+                className="trigger svg-margin"
+                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                onClick={this.toggle}
+              />
+            </Header>
+            <Content
+              style={{
+                margin: '24px 16px',
+                padding: 24,
+                background: '#fff',
+                minHeight: 280,
+              }}
+            >
+              Content
+            </Content>
+          </Layout>
+        </Layout>
+      );
+    }
+  }
+
 
 export default WaiterUI;
