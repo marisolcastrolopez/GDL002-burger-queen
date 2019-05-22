dbce3c534ca49d27ac5ef307c1a016b027fd997d
marisolcastrolopez <solcastrodg@gmail.com>
2019-05-22 00:10:49 -0500

    Waiter UI development

--- src/components/WaiterUI.js
+++ src/components/WaiterUI.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import "antd/dist/antd.css";
-import { Layout, Menu, Icon, Button, Row, Col } from 'antd';
+import { Link } from "react-router-dom";
+import { Layout, Menu, Icon, Button, Row, Col, Dropdown, Avatar } from 'antd';
 
 const { Header, Sider, Content } = Layout;
+
+
+const menu = () => {
+  return (
+      <Menu>
+          <Menu.Item key="0">
+            <Link to="/waiterlist">Change to Chef View</Link>
+          </Menu.Item>
+          <Menu.Item key="1">
+            <Link to="/waiterlist">Change User</Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+          <Link to="/waiterlist">Log out</Link>
+          </Menu.Item>
+      </Menu>
+  )
+};
+
+const RenderMenu = () => {
+  return (
+      <Dropdown overlay={menu} trigger={['click']}>
+          <a className="ant-dropdown-link" href="#">
+          <Avatar size="large" icon="user" />   Waiter Name <Icon type="down" />
+          </a>
+      </Dropdown>
+  )
+}
+
+
 
 class WaiterUI extends React.Component {
   state = {
@@ -45,26 +75,85 @@ class WaiterUI extends React.Component {
                   onClick={this.toggle}
                 />
               </Col>
-              <Col className="center-col" md={19} lg={19} xl={19}>
-                
+              <Col className="center-col" md={17} lg={17} xl={17}>
+              
               </Col>
               <Col className="center-col" md={2} lg={2} xl={2}>
-                <Button type="primary">Primary</Button>
+                <Button type="primary">New Order</Button>
               </Col>
-              <Col className="center-col" md={2} lg={2} xl={2}>
-                None
+              <Col className="center-col" md={4} lg={4} xl={4}>
+              <RenderMenu />
               </Col>
             </Row>
           </Header>
           <Content
             style={{
               margin: '24px 16px',
-              padding: 24,
+              padding: 20,
               background: '#fff',
               minHeight: 280,
             }}
           >
-            Content
+            <Col scroll={{ y: true }} md={12} lg={12} xl={12}>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/> <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+                <Button type="primary">New Order</Button>
+                <br/>
+              </Col>
+              <Col md={12} lg={12} xl={12}>
+                <Button type="primary">New Order</Button>
+              </Col>
             </Content>
         </Layout>
       </Layout>
